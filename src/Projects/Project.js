e6173d79dfeeeb19338c23013887fa9e2b894fa1
prathamg108 <pratham.gandhi20@pccoepune.org>
2023-03-01 11:56:06 +0530

    commit

--- src/Projects/Project.js
+++ src/Projects/Project.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import './Project.css'
+import React from "react";
+import { text } from "./ProjectText";
+import "./Project.css";
 const Project = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <>
+      <div>Projects</div>
+      <div className="project">
+        {text.map((proj) => (
+          <div className="list" key={proj.topic}>
+            <div className="card">
+              <div className="card_image">
+                <img src="{proj.imaUrl}" alt="photoss" />
+              </div>
+              <div className="img_txt">{proj.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Project;
--- src/Projects/ProjectText.js
+++ src/Projects/ProjectText.js
@@ -2,28 +2,28 @@ export const text = [
     {
         imgUrl:" ",
         topic:"iNoteBook",
-        describtion:"MERN stack project help to maintain the notes ",
+        description:"MERN stack project help to maintain the notes ",
         language:"NodeJs   ReactJs",
         github:""
     },
     {
         imgUrl:" ",
         topic:"DailyDose",
-        describtion:"Used to track the Daily News on website",
+        description:"Used to track the Daily News on website",
         language:"ReactJs",
         github:""
     },
     {
         imgUrl:" ",
         topic:"Gym n diffent Page",
-        describtion:"various animated Pages created using html n css ",
+        description:"various animated Pages created using html n css ",
         language:"HTML   CSS",
         github:""
     },
     {
         imgUrl:" ",
         topic:"PhoneBook",
-        describtion:"C project that work as phone diary ",
+        description:"C project that work as phone diary ",
         language:"C  DSA",
         github:""
     },
